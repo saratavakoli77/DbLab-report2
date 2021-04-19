6c05e8e3a7c34b1b6a9cb21a7deabb70eb33ffed
saratvk <saratvk1377@gmail.com>
2021-04-19 22:17:05 +0430

    feat: add jobseekers to main and app.module

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // This Line Must Be Added
   const options = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('jobseekers')
+    .setDescription('jobseekers_api')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('jobseeker')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
